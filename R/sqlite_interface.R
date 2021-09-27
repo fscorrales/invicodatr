260e819cf8bb6f26a2e7b804360943f16504dc2f
Fernando Corrales <corrales_fernando@hotmail.com>
2021-09-27 14:39:15 -0300

    connect_sqlite modified

--- R/sqlite_interface.R
+++ R/sqlite_interface.R
@@ -3,9 +3,12 @@
 #' Connect to a SQLite database in an specif path (/R Output/SQLite Files/).
 #'
 #' @param sqlite_name SQLite database to connect
+#' @param ... authentication arguments needed by the DBMS instance; these
+#'  typically include user, password, host, port, dbname, etc. For details
+#'  see the appropriate DBIDriver
 #'
 #' @export
-connect_sqlite <-function(sqlite_name){
+connect_sqlite <-function(sqlite_name, ...) {
 
   file_path <- paste0(output_path(), "/SQLite Files/",
                       sqlite_name, ".sqlite")
@@ -13,6 +16,17 @@ connect_sqlite <-function(sqlite_name){
   #Use dbCanConnect() to check if a connection can be established.
 
   con <- DBI::dbConnect(RSQLite::SQLite(),
-                        dbname = file_path)
+                        dbname = file_path, ...)
+
+}
+
+write_sqlite <- function(sqlite_name, table_name,
+                         df, append = FALSE,
+                         overwrite = FALSE, ...) {
+
+  con <- connect_sqlite(sqlite_name)
+  DBI::dbWriteTable(con, name = table_name, value = df,
+                    append = append, overwrite = overwrite, ...)
+  DBI::dbDisconnect(con)
 
 }
