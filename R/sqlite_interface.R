3ee358bbd4901274734d9784f266ac047835adaa
Fernando Corrales <corrales_fernando@hotmail.com>
2021-09-27 15:04:32 -0300

    write_sqlite created

--- R/sqlite_interface.R
+++ R/sqlite_interface.R
@@ -1,6 +1,6 @@
 #' Create a connection to a SQLite database
 #'
-#' Connect to a SQLite database in an specif path (/R Output/SQLite Files/).
+#' Connect to a SQLite database in an specific path (/R Output/SQLite Files/).
 #'
 #' @param sqlite_name SQLite database to connect
 #' @param ... authentication arguments needed by the DBMS instance; these
@@ -20,13 +20,26 @@ connect_sqlite <-function(sqlite_name, ...) {
 
 }
 
-write_sqlite <- function(sqlite_name, table_name,
-                         df, append = FALSE,
-                         overwrite = FALSE, ...) {
+#' Copy data frames to SQLite database
+#'
+#' Writes, overwrites or appends a data frame to SQLite database in an
+#'  specific path (/R Output/SQLite Files/).
+#'
+#' @param sqlite_name A character string specifying the unquoted SQLite
+#'  database name
+#' @param table_name A character string specifying the unquoted SQLite
+#'  table name
+#' @param df A data.frame (or coercible to data.frame)
+#' @param ... Other parameters passed on to
+#'  \code{\link[DBI]{dbWriteTable}}.
+#'
+#' @export
+write_sqlite <- function(sqlite_name,
+                         table_name,
+                         df, ...) {
 
   con <- connect_sqlite(sqlite_name)
-  DBI::dbWriteTable(con, name = table_name, value = df,
-                    append = append, overwrite = overwrite, ...)
+  DBI::dbWriteTable(con, name = table_name, value = df, ...)
   DBI::dbDisconnect(con)
 
 }
