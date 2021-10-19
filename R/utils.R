d0f3b148ffba04ae24c9612c7deaa425e78bda7a
Fernando INVICO <fcorrales@invico.gov.ar>
2021-10-19 12:32:43 -0300

    Add vroom package

--- R/utils.R
+++ R/utils.R
@@ -26,6 +26,7 @@ write_csv <- function(dt, file_name){
   file_full_name <- paste0(output_path(), "/CSV Files/",
                      file_name)
 
-  readr::write_excel_csv(dt, file_full_name)
+  # readr::write_excel_csv(dt, file_full_name)
+  vroom::vroom_write(dt, file_full_name, delim = ",")
 
 }
