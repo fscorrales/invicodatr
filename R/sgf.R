50baee15b1214c44ace29667f09a865c2f20dd2a
Fernando INVICO <fcorrales@invico.gov.ar>
2021-10-20 13:05:34 -0300

    read_sgf_resumen_rend_prov incomplete

---
+++ R/sgf.R
@@ -0,0 +1,32 @@
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+NULL
+
+#' Read, process and write SSCC's Movimientos Banco report
+#'
+#' Returns a cleaned tibble version of SSCC's report. Also, a csv and sqlite
+#'  file could be exported.
+#'
+#' @param path a character vector of full path names. If more than one path is
+#'  assigned, a dataframe that combine all of them will be return.
+#' @param write_csv logical. Should a csv file be generated?
+#' @param write_sqlite logical. Should a sqlite file be generated?
+#'
+#' @export
+rpw_sgf_resumen_rend_prov <- function(path, write_csv = FALSE,
+                                  write_sqlite = FALSE){
+
+  Ans <- purrr::map_df(path, ~ try_read(read_sgf_resumen_rend_prov(.x)))
+
+  if (write_csv == TRUE) {
+    write_csv(Ans, "Resumen Rendiciones por Proveedor SGF.csv")
+  }
+
+  if (write_sqlite == TRUE) {
+    write_sqlite("SGF", "resumen_rend_prov",
+                 df = Ans, overwrite = TRUE)
+  }
+
+  Ans
+
+}
---
+++ tests/testthat/test-sgf_fct_read.R
@@ -0,0 +1,8 @@
+test_that("reading resumen_rend_prov returns a tibble", {
+  df <- system.file("extdata", "resumen_rend_prov.csv",
+                    package = "invicodatr", mustWork = TRUE) %>%
+    read_sgf_resumen_rend_prov()
+  expect_s3_class(df, c("tbl_df", "tbl", "data.frame"))
+  # expect_equal(ncol(df), 12)
+
+})
