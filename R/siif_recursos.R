311b6308c9ca6bcf1acfbd1ceea4109e492791c4
Fernando Corrales <corrales_fernando@hotmail.com>
2021-11-26 19:34:39 -0300

    Add append parameter to rpw_siif_comprobantes_rec

--- R/siif_recursos.R
+++ R/siif_recursos.R
@@ -11,22 +11,44 @@ NULL
 #'  assigned, a dataframe that combine all of them will be return.
 #' @param write_csv logical. Should a csv file be generated?
 #' @param write_sqlite logical. Should a sqlite file be generated?
+#' @param overwrite_sql logical. Should the sql table be overwritten? Default
+#'  set to FALSE meaning that the table would be appended.
 #'
 #' @export
 rpw_siif_comprobantes_rec <- function(path = NULL, write_csv = FALSE,
-                                   write_sqlite = FALSE){
+                                      write_sqlite = FALSE,
+                                      overwrite_sql = FALSE){
 
   Ans <- purrr::map_df(path, ~ try_read(read_siif_comprobantes_rec_rci02(.x)))
 
   if (nrow(Ans) != 0 ) {
+
     if (write_csv == TRUE) {
       write_csv(Ans, "Comprobantes Recursos Ingresados SIIF (rci02).csv")
     }
 
     if (write_sqlite == TRUE) {
-      write_sqlite("siif", "comprobantes_rec_rci02",
-                   df = Ans, overwrite = TRUE)
+
+      sql_db <- "siif"
+      sql_table <- "comprobantes_rec_rci02"
+      sql_key_var <- "ejercicio"
+
+      if (overwrite_sql == TRUE) {
+        write_sqlite(sql_db, sql_table,
+                     df = Ans, overwrite = TRUE)
+      } else {
+        filter_var <- dplyr::select(Ans, .data[[sql_key_var]]) %>%
+          unique()
+        execute_sqlite(sql_db,
+                       paste0("DELETE FROM ", sql_table, " ",
+                              "WHERE ejercicio = ?"),
+                       params = list(filter_var[[sql_key_var]]))
+        write_sqlite(sql_db, sql_table,
+                     df = Ans, append = TRUE)
+      }
+
     }
+
   }
 
   invisible(Ans)
