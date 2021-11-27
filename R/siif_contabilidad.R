ece22653b5b9b8aaef5246e19e8ea07b1a547010
Fernando Corrales <corrales_fernando@hotmail.com>
2021-11-27 12:18:15 -0300

    Add append parameter to rpw_siif_mayor_contable

--- R/siif_contabilidad.R
+++ R/siif_contabilidad.R
@@ -11,10 +11,13 @@ NULL
 #'  assigned, a dataframe that combine all of them will be return.
 #' @param write_csv logical. Should a csv file be generated?
 #' @param write_sqlite logical. Should a sqlite file be generated?
+#' @param overwrite_sql logical. Should the sql table be overwritten? Default
+#'  set to FALSE meaning that the table would be appended.
 #'
 #' @export
 rpw_siif_mayor_contable <- function(path = NULL, write_csv = FALSE,
-                                      write_sqlite = FALSE){
+                                    write_sqlite = FALSE,
+                                    overwrite_sql = FALSE){
 
   Ans <- purrr::map_df(path, ~ try_read(read_siif_mayor_contable_rcocc31(.x)))
 
@@ -24,9 +27,31 @@ rpw_siif_mayor_contable <- function(path = NULL, write_csv = FALSE,
     }
 
     if (write_sqlite == TRUE) {
-      write_sqlite("siif", "mayor_contable_rcocc31",
-                   df = Ans, overwrite = TRUE)
+
+      sql_db <- "siif"
+      sql_table <- "mayor_contable_rcocc31"
+      sql_key_var_1 <- "mes"
+      sql_key_var_2 <- "cta_contable"
+
+      if (overwrite_sql == TRUE) {
+        write_sqlite(sql_db, sql_table,
+                     df = Ans, overwrite = TRUE)
+      } else {
+        filter_var <- dplyr::select(Ans, .data[[sql_key_var_1]],
+                                    .data[[sql_key_var_2]]) %>%
+          unique()
+        execute_sqlite(sql_db,
+                       paste0("DELETE FROM ", sql_table, " ",
+                              "WHERE mes = ? ",
+                              "AND cta_contable = ?"),
+                       params = list(filter_var[[sql_key_var_1]],
+                                     filter_var[[sql_key_var_2]]))
+        write_sqlite(sql_db, sql_table,
+                     df = Ans, append = TRUE)
+      }
+
     }
+
   }
 
   invisible(Ans)
